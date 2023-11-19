f4be54a33d586d1d4290fca9068f95e8d38ed25a
julio <jcolladopm7@gmail.com>
2023-11-19 10:38:54 -0500

    added code visualization

--- TextFieldSODemo/ContentView.swift
+++ TextFieldSODemo/ContentView.swift
@@ -23,48 +23,57 @@ struct ContentView: View {
         case fourth
     }
     
+    private var code: String {
+        return char01 + char02 + char03 + char04
+    }
+    
     var body: some View {
         ZStack {
             Color.gray.opacity(0.2).ignoresSafeArea()
-            HStack(alignment: .center ,spacing: 8) {
-                Group {
-                    CustomizedTextField(text: $char01, textAlignment: .center , onDeleteBackward: {
-                        nextResponder = .none
-                    })
-                    .focused($nextResponder, equals: .first)
-                    
-                    CustomizedTextField(text: $char02, textAlignment: .center, onDeleteBackward: {
-                        nextResponder = .first
-                    })
-                    .focused($nextResponder, equals: .second)
-                    
-                    CustomizedTextField(text: $char03, textAlignment: .center, onDeleteBackward: {
-                        nextResponder = .second
-                    })
-                    .focused($nextResponder, equals: .third)
-                    
-                    CustomizedTextField(text: $char04, textAlignment: .center, onDeleteBackward: {
-                        nextResponder = .third
-                    })
-                    .focused($nextResponder, equals: .fourth)
+            VStack {
+                HStack(alignment: .center ,spacing: 8) {
+                    Group {
+                        CustomizedTextField(text: $char01, textAlignment: .center , onDeleteBackward: {
+                            nextResponder = .none
+                        })
+                        .focused($nextResponder, equals: .first)
+                        
+                        CustomizedTextField(text: $char02, textAlignment: .center, onDeleteBackward: {
+                            nextResponder = .first
+                        })
+                        .focused($nextResponder, equals: .second)
+                        
+                        CustomizedTextField(text: $char03, textAlignment: .center, onDeleteBackward: {
+                            nextResponder = .second
+                        })
+                        .focused($nextResponder, equals: .third)
+                        
+                        CustomizedTextField(text: $char04, textAlignment: .center, onDeleteBackward: {
+                            nextResponder = .third
+                        })
+                        .focused($nextResponder, equals: .fourth)
+                    }
+                    .frame(width: 44, height: 57)
+                    .font(.system(size: 42))
+                    .background(.white)
+                    .cornerRadius(10)
                 }
-                .frame(width: 44, height: 57)
-                .font(.system(size: 42))
-                .background(.white)
-                .cornerRadius(10)
+                .onChange(of: char01, { oldValue, newValue in
+                    switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
+                })
+                .onChange(of: char02, { oldValue, newValue in
+                    switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
+                })
+                .onChange(of: char03, { oldValue, newValue in
+                    switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
+                })
+                .onChange(of: char04, { oldValue, newValue in
+                    switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
+                })
+                
+                Text("Code: \(code)")
+                    .padding()
             }
-            .onChange(of: char01, { oldValue, newValue in
-                switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
-            })
-            .onChange(of: char02, { oldValue, newValue in
-                switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
-            })
-            .onChange(of: char03, { oldValue, newValue in
-                switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
-            })
-            .onChange(of: char04, { oldValue, newValue in
-                switchFieldIfNeeded(oldValue: oldValue, newValue: newValue)
-            })
         }
     }
     
